5d86b62c108896308b3f23529fe0a2fe4fd25fac
Hyeyeon-Ko <khy33355@gmail.com>
2023-12-22 14:56:47 +0900

    화면 클릭 이벤트 - 화면전환(Intent)

--- bts_lecture/app/src/main/java/com/example/bts_lecture/MainActivity.kt
+++ bts_lecture/app/src/main/java/com/example/bts_lecture/MainActivity.kt
@@ -1,13 +1,64 @@
 package com.example.bts_lecture
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1. 화면이 클릭되었다는 것을 프로그램이 알아야 한다.
+        val image1 = findViewById<ImageView>(R.id.bts_image_1) // bts_image_1 가져오기
+        val image2 = findViewById<ImageView>(R.id.bts_image_2)
+        val image3 = findViewById<ImageView>(R.id.bts_image_3)
+        val image4 = findViewById<ImageView>(R.id.bts_image_4)
+        val image5 = findViewById<ImageView>(R.id.bts_image_5)
+        val image6 = findViewById<ImageView>(R.id.bts_image_6)
+        val image7 = findViewById<ImageView>(R.id.bts_image_7)
+
+        image1.setOnClickListener {  // 1번 이미지를 클릭하면
+
+            Toast.makeText(this, "1번 클릭 완료", Toast.LENGTH_LONG).show() // "1번 클릭 완료" 띄우기
+
+            // 2. 화면을 클릭하면, 사진을 크게 보여준다.
+            var intent = Intent(this, Bts1Activity::class.java)
+            startActivity(intent)
+        }
+
+        image2.setOnClickListener {
+            var intent = Intent(this, Bts2Activity::class.java)
+            startActivity(intent)
+        }
+
+        image3.setOnClickListener {
+            var intent = Intent(this, Bts3Activity::class.java)
+            startActivity(intent)
+        }
+
+        image4.setOnClickListener {
+            var intent = Intent(this, Bts4Activity::class.java)
+            startActivity(intent)
+        }
+
+        image5.setOnClickListener {
+            var intent = Intent(this, Bts5Activity::class.java)
+            startActivity(intent)
+        }
+
+        image6.setOnClickListener {
+            var intent = Intent(this, Bts6Activity::class.java)
+            startActivity(intent)
+        }
+
+        image7.setOnClickListener {
+            var intent = Intent(this, Bts7Activity::class.java)
+            startActivity(intent)
+        }
 
     }
 }

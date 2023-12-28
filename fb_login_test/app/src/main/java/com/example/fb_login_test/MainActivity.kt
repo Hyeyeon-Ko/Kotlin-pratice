40e71f247bba16d47841bbde3b0f2886773aa0e1
Hyeyeon-Ko <khy33355@gmail.com>
2023-12-28 17:43:51 +0900

    Firebase 익명로그인 구현

--- fb_login_test/app/src/main/java/com/example/fb_login_test/MainActivity.kt
+++ fb_login_test/app/src/main/java/com/example/fb_login_test/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.fb_login_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
+        val btn = findViewById<Button>(R.id.noEmailLoginBtn)
+        btn.setOnClickListener {
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        Log.d("MainAtivity", user!!.uid)
+                    } else {
+                        Toast.makeText(baseContext, "Authentication Failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
     }
 }

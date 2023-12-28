231157f27dbe35cb17b4c3ef02fa8a524f0e7729
Hyeyeon-Ko <khy33355@gmail.com>
2023-12-29 00:34:14 +0900

    Splash 구현

--- diet_memo/app/build.gradle.kts
+++ diet_memo/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.diet_memo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.diet_memo"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 

11e54b94142a50c4f65e5bde3e914cbc20bc150d
Hyeyeon-Ko <khy33355@gmail.com>
2023-12-30 23:18:28 +0900

    DB에 북마크 정보 저장하기

--- mango_contents/app/src/main/java/com/example/mango_contents/MainActivity.kt
+++ mango_contents/app/src/main/java/com/example/mango_contents/MainActivity.kt
@@ -123,6 +123,8 @@ class MainActivity : AppCompatActivity() {
 
                 val intent = Intent(baseContext, ViewActivity::class.java)
                 intent.putExtra("url", items[position].url)
+                intent.putExtra("imageUrl", items[position].imageUrl)
+                intent.putExtra("title", items[position].titleText)
                 startActivity(intent)
 
             }
